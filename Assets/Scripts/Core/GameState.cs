fc7518f6a0125cc0a73ee1b30fa4e91a9ddf9efc
Maria Barraza <reikafer@gmail.com>
2020-02-09 22:24:59 -0700

    Gameplay Mechanics, Sound Manager, and Save State

---
+++ Assets/Scripts/Core/GameState.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameState : MonoBehaviour
+{
+    public float speed = 10f;
+    public bool completedFirstLevel = false;
+    public bool completedSecondLevel = false;
+    public bool completedThirdLevel = false;
+    public string scenename;
+    public Scene scene;
+    
+    public void SaveGameState()
+    {
+        SaveSystem.SaveGameState(this);
+    }
+
+    public void LoadGameState()
+    {
+        GameStateData data = SaveSystem.LoadGameState();
+
+        SceneManager.LoadScene(data.scenename);
+        
+        completedFirstLevel = data.completedFirstLevel;
+        completedSecondLevel = data.completedSecondLevel;
+        completedThirdLevel = data.completedThirdLevel;
+    }
+    private void Update() 
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log("Guardado Exitoso");
+            SaveGameState();
+        }
+        else if (Input.GetKeyDown(KeyCode.L))
+        {
+            Debug.Log("Carga Exitosa");
+            LoadGameState();
+        }
+    }
+}
