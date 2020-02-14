768d8bc9b9333a4694d2c75d07679fd7737961c5
Javier Eliud <javiereliudliz@gmail.com>
2020-02-14 16:50:38 -0700

    Script de manos para el jefe

---
+++ Assets/Scripts/Manos.cs
@@ -0,0 +1,43 @@
+﻿ using System.Collections;
+ using UnityEngine;
+ 
+ public class Manos : MonoBehaviour
+ {
+     private float movementDuration = 1f;
+     private float waitBeforeMoving = 1f;
+     private bool hasArrived = false;
+
+     private void Update()
+     {
+         if(!hasArrived)
+         {
+             hasArrived = true;
+             float randX = Random.Range(-5.0f, 5.0f);
+             float randY = Random.Range(-5.0f, 5.0f);
+             StartCoroutine(MoveToPoint(new Vector2(randX, -randY)));
+         }
+     }
+
+    /// <summary>
+    /// Makes the object move in random directions.
+    /// </summary>
+    /// <param="targetPos">Vector2 con la posición a moverse<param>
+     private IEnumerator MoveToPoint(Vector2 targetPos)
+     {
+         float timer = 0.0f;
+         Vector2 startPos = transform.position;
+ 
+         while (timer < movementDuration)
+         {
+             timer += Time.deltaTime;
+             float t = timer / movementDuration;
+             t = t * t * t * (t * (6f * t - 15f) + 10f);
+             transform.position = Vector2.Lerp(startPos, targetPos, t);
+ 
+             yield return null;
+         }
+ 
+         yield return new WaitForSeconds(waitBeforeMoving);
+         hasArrived = false;
+     }
+ }
